78e1d4073608465e4f804f7686d814c85974020e
Alexandra Buck <alkimbu@Alexandras-MBP.fritz.box>
2020-05-29 13:02:03 +0200

    provider und main.js verlinkt

---
+++ main.js
@@ -0,0 +1,9 @@
+let startLayer = L.tileLayer.provider("BasemapAT.grau");
+
+let map = L.map("map", {
+    center: [48.208333, 16.373056],
+    zoom: 12,
+    layers: [
+        startLayer
+    ]
+});
