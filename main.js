9ce560b849001ab6a0eebb70e120a328fd3392d0
Alexandra Buck <alkimbu@Alexandras-MBP.fritz.box>
2020-06-05 09:56:39 +0200

    Zusammenführen

--- main.js
+++ main.js
@@ -21,9 +21,5 @@ for (const start of startpoints) {
 L.marker([start.lat, start.lng]).addTo(map);
     startmarker.bindPopup(`Tour ${start.name}`)
 }
-<<<<<<< HEAD
 overlay.startpoints.addTo(map)
 
-=======
-overlay.startpoints.addTo(map)
->>>>>>> a8b9e8ec5674e119ebc171a46ee7e493a9c7fe8f
