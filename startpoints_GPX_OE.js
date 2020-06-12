4628dde5b6b0d6b69ce5dda64c479398fb2b0305
lanbra <laura.brandstetter@web.de>
2020-06-12 13:46:17 +0200

    dynamischer Link zu touren

--- startpoints_GPX_OE.js
+++ startpoints_GPX_OE.js
@@ -3,55 +3,63 @@ const startpoints = [{
         "name": "Bärenkopf",
         "lat": 47.429732,
         "lng": 11.718545,
-        "schwierigkeit": "schwer"
+        "schwierigkeit": "schwer",
+        "tourhtml":"http://ibkdrei.github.io/Achensee.html"
     },
     {
         "id": 2,
         "name": "Dachstein Klettersteig",
         "lat": 47.450492,
         "lng": 13.61795,
-        "schwierigkeit": "schwer"
+        "schwierigkeit": "schwer",
+        "tourhtml":"http://ibkdrei.github.io/Dachstein.html"
     },
     {
         "id": 3,
         "name": "Hallstatt",
         "lat": 47.556994,
         "lng": 13.647466,
-        "schwierigkeit": "mittel"
+        "schwierigkeit": "mittel",
+        "tourhtml":"http://ibkdrei.github.io/Hallstatt.html"
     },
     {
         "id": 4,
         "name": "Innsbruck City Tour",
         "lat": 47.26756,
         "lng": 11.39076,
-        "schwierigkeit": "leicht"
+        "schwierigkeit": "leicht",
+        "tourhtml":"http://ibkdrei.github.io/InnsbruckCitytour.html"
     },
     {
         "id": 5,
         "name": "Innsbruck Karwendel",
         "lat": 47.311928,
         "lng": 11.383676,
-        "schwierigkeit": "mittel"
+        "schwierigkeit": "mittel",
+        "tourhtml":"http://ibkdrei.github.io/InnsbruckKarwendel.html"
     },
     {
         "id": 6,
         "name": "Seebensee",
         "lat": 47.387982,
         "lng": 10.937963,
-        "schwierigkeit": "schwer"
+        "schwierigkeit": "schwer",
+        "tourhtml":"http://ibkdrei.github.io/Seebensee.html"
     },
     {
         "id": 7,
         "name": "Zell am See",
         "lat": 47.334159,
         "lng": 12.846814,
-        "schwierigkeit": "schwer"
+        "schwierigkeit": "schwer",
+        "tourhtml":"http://ibkdrei.github.io/ZellamSee.html"
     },
     {
         "id": 8,
         "name": "Olperer Haus Hängebrücke",
         "lat": 47.038532,
         "lng": 11.701643,
-        "schwierigkeit": "mittel"
+        "schwierigkeit": "mittel",
+        "tourhtml":"http://ibkdrei.github.io/Olpererhuette.html"
     }
 ]
